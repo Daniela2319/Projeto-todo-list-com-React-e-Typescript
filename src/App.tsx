d51cb1b25fa85f78767c4944013f393272247980
Daniela2319 <frankvt04@gmail.com>
2024-07-05 21:01:49 -0300

    Continuando aplicação na to-do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Header } from "./components/Header/header";
 import "./styles/global.css";
+import { Tasks } from "./components/Tasks/Tasks";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />;
+      <Tasks />
+    </>
+  );
 }
 
 export default App;
